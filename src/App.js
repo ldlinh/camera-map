058516ec7eb165fa08d4bb36cc30a40f440a32a7
TuanMaiChung <mctuan@mmsofts.com>
2020-01-10 09:35:26 +0700

    handle directionsService and add multiple DirectionsRenderer

--- src/App.js
+++ src/App.js
@@ -1,78 +1,118 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import camera from './Monitoring.png';
 import './App.css';
-import ReactDOM from "react-dom";
-import { compose, withProps } from "recompose";
+import {compose, withProps, lifecycle} from "recompose";
 import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker,
+    DirectionsRenderer
 } from "react-google-maps";
 import Modal from './components/Modal'
-// import jsmpeg from 'jsmpeg';
-
 
 const App = () => {
 
-  const [openModal, setOpenModal] = useState(false)
+    let [markerList, setMakertList] = useState([
+        {lat: 10.7624939, lng: 106.7020087},
+        {lat: 10.730930, lng: 106.678224},
+        {lat: 10.754230, lng: 106.678125},
+        {lat: 10.773106, lng: 106.637726}
+    ]);
+    const [openModal, setOpenModal] = useState(false);
 
-  const onMarkerClick = () => {
-    setOpenModal(true)
-    window.showCamera("ws://localhost:9999")
-  };
+    const onMarkerClick = () => {
+        setOpenModal(true);
+        window.showCamera("ws://localhost:9999")
+    };
 
-  const resetModal = () => {
-    setOpenModal(false)
-  }
+    const resetModal = () => {
+        setOpenModal(false)
+    };
 
-  const GoogleMapCompoinent = compose(
-    withProps({
-      googleMapURL:
-        "https://maps.googleapis.com/maps/api/js?key=AIzaSyDiInU8M5pi0ATUaLXFgdX95fkvkJD2nKM&v=3.exp&libraries=geometry,drawing,places",
-      loadingElement: <div style={{ height: `100%` }} />,
-      containerElement: <div style={{ height: `768px` }} />,
-      mapElement: <div style={{ height: `100%` }} />
-    }),
-    withScriptjs,
-    withGoogleMap,
-  )(props => (
-    <GoogleMap
-      defaultZoom={18}
-      defaultCenter={{ lat: 10.7624939, lng: 106.7020087 }}>
-      {props.isMarkerShown && (
-        <Marker
-          name={'MMSoftDev'}
-          position={{ lat: 10.7624939, lng: 106.7020087 }}
-          title={'MMSoft Development Room'}
-          icon={{
-            url: camera
-          }}
-          onClick={onMarkerClick}
-        />
-      )}
-    </GoogleMap>
 
-  ));
+    useEffect(() => {
+    }, [])
 
-  return (
-    <div className="App">
-      <header></header>
-      <body>
-        <div>
-          <GoogleMapCompoinent isMarkerShown="true"
-          />
+    const GoogleMapCompoinent = compose(
+        withProps({
+            googleMapURL:
+                "https://maps.googleapis.com/maps/api/js?key=AIzaSyDiInU8M5pi0ATUaLXFgdX95fkvkJD2nKM&v=3.exp&libraries=geometry,drawing,places",
+            loadingElement: <div style={{height: `100%`}}/>,
+            containerElement: <div style={{height: `768px`}}/>,
+            mapElement: <div style={{height: `100%`}}/>
+        }),
+        withScriptjs,
+        withGoogleMap,
+        lifecycle({
 
+            componentDidMount() {
+                const google = window.google;
+                const waypoints = markerList.map(p => ({
+                    location: {lat: p.lat, lng: p.lng},
+                    stopover: true
+                }));
+                const origin = waypoints.shift().location;
+                const destination = waypoints.pop().location;
+                const directionsService = new google.maps.DirectionsService();
+                directionsService.route(
+                    {
+                        origin: origin,
+                        destination: destination,
+                        travelMode: google.maps.TravelMode.DRIVING,
+                        waypoints: waypoints
+                    },
+                    (result, status) => {
+                        if (status === google.maps.DirectionsStatus.OK) {
+                            this.setState({
+                                directions: result
+                            });
+                        } else {
+                            this.setState({error: result});
+                        }
+                    }
+                );
+            }
+        })
+    )(props => (
+        <GoogleMap
+            defaultZoom={18}
+            defaultCenter={{lat: 10.7624939, lng: 106.7020087}}>
+            {props.isMarkerShown && (
+                props.markerList.map(marker =>
+                    <Marker
+                        name={'MMSoftDev'}
+                        position={{lat: marker.lat, lng: marker.lng}}
+                        title={'MMSoft Development Room'}
+                        icon={{url: camera}}
+                        onClick={props.openDialog}
+                    />
+                )
+            )}
+            {
+                props.directions &&
+                <DirectionsRenderer
+                    directions={props.directions}
+                />
+            }
+        </GoogleMap>
+    ));
+
+    return (
+        <div className="App">
+            <div>
+                <GoogleMapCompoinent
+                    isMarkerShown="true"
+                    markerList={markerList}
+                    openDialog={onMarkerClick}
+                />
+            </div>
+            <div>
+                <Modal openModal={openModal} resetModal={resetModal}/>
+            </div>
         </div>
-        <div>
-          <Modal openModal={openModal} resetModal={resetModal} />
-        </div>
-      </body>
-    </div>
 
-
-  );
-}
+    );
+};
 
 export default App;
